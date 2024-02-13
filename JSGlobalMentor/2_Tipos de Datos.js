863c68634f9077f6093caa3cef9dfa664536d6f8
Iceisland12 <luisguile495@gmail.com>
2024-02-13 14:17:50 -0400

    Update

--- JSGlobalMentor/2_Tipos de Datos.js
+++ JSGlobalMentor/2_Tipos de Datos.js
@@ -1,3 +1,23 @@
 let miEntero = 10;
 console.log(miEntero);
+console.log(typeof miEntero)
+
+let miFloat = 10.4;
+console.log(miFloat);
+
+let miString = 'Hola';
+console.log(miString);
+console.log(typeof miString)
+
+let miBoolean = false;
+console.log(miBoolean);
+console.log(typeof miBoolean);
+
+let miNull = null;
+console.log(miNull);
+console.log(typeof miNull);
+
+let miUndefined = undefined;
+console.log(miUndefined);
+console.log(typeof miUndefined);
 
---
+++ JSGlobalMentor/3_Hoisting.js
@@ -0,0 +1,9 @@
+// Hoisting
+
+let x; // Declaramos la variable
+x = 10; // Inicializamos la variable;
+
+y = 2;
+
+console.log(x);
+var y; 
